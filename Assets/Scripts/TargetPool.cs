60f8c6c3ef4f9b8cef4a34b342663e9e34dc3c31
Adam Frost <78973950+adfr05t@users.noreply.github.com>
2023-03-18 11:21:04 +0000

    Add TargetPool.cs

---
+++ Assets/Scripts/TargetPool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPool : MonoBehaviour
+{
+    [SerializeField] private Target targetPrefab;
+    private Queue<Target> targetQueue = new Queue<Target>();
+
+    public static TargetPool Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Target Get()
+    {
+        if (targetQueue.Count == 0)
+        {
+            AddTargets(1);
+        }
+
+        return targetQueue.Dequeue();
+    }
+
+    void AddTargets(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Target target = Instantiate(targetPrefab);
+            target.gameObject.SetActive(false);
+            targetQueue.Enqueue(target);
+        }
+    }
+
+    public void ReturnToPool(Target target)
+    {
+        target.gameObject.SetActive(false);
+        targetQueue.Enqueue(target);
+    }
+  
+}
